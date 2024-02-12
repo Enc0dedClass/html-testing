0a6d08c209b8a2cc7171e1a0675cd6314e9e5ce8
Enc0dedClass_scpc <aronshoey@gmail.com>
2024-02-12 12:02:37 +0100

    added file requests

--- html_test_1/scripts/main.js
+++ html_test_1/scripts/main.js
@@ -1,3 +1,19 @@
-const products = ""
+async function updateProducts()
+{
+    const productFolder = "https://github.com/Enc0dedClass/html-testing/tree/main/html_test_1/products/rema1000/";
+    
+    
+    const htmlThing = document.getElementsByClassName("dynamicTest");
+    
+    for (let i in htmlThing)
+    {
+        let product = productFolder + "p" + (i).toString()
+        const getProduct = new Request(product);
+        const response = await fetch(getProduct);
+        const myText = await response.text;
+        htmlThing[i].textContent = myText.toString();
+        console.log(product + " is: " + myText.toString());
+    }
+}
 
-let x = 
+updateProducts();
