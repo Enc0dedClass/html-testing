ae7cb65ef977567679c76551c0855b59bd7c97cf
Enc0dedClass_scpc <aronshoey@gmail.com>
2024-02-12 12:34:10 +0100

    updated path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function loadPage1()
 {
     console.log("going to page 1");
-    window.location.href = "htmlFiles/shopSiteTest.html";
+    window.location.href = "html_test_1/htmlFiles/shopSiteTest.html";
 }
